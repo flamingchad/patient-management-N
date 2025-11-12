0e90561b4b01d8f31677a2f51a99be6ace329066
Nithiesh Mohan <nithiesh164@gmail.com>
2025-11-12 14:01:51 +0530

    Impl repository

--- patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -1,4 +1,11 @@
 package com.pm.patientservice.repository;
 
-public interface PatientRepository {
+import com.pm.patientservice.model.Patient;
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
 }
