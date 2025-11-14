0933fb44d2d5f37386ae5a18cfe042844ac22344
Nithiesh Mohan <nithiesh164@gmail.com>
2025-11-14 14:05:54 +0530

    Patient Mapper is modified to handle PatientRequestDTO

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 
@@ -17,14 +18,14 @@ public class PatientMapper {
         return patientDTO;
     }
 
-//    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
-//        Patient patient = new Patient();
-//        patient.setName(patientRequestDTO.getName());
-//        patient.setAddress(patientRequestDTO.getAddress());
-//        patient.setEmail(patientRequestDTO.setEmail());
-//        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
-//        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
-//
-//        return patient;
-//    }
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
+        return patient;
+    }
 }
